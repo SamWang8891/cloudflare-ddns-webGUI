8a8a7ed98c428e8563c15f735f8458ef91116bf9
SamWang8891 <g348.8891@gmail.com>
2024-10-30 09:38:02 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,16 +2,25 @@
 
 
 # Create the file to store log
-LOG_FILE=./app/script/.env
-touch "$LOG_FILE"
-echo 'record="sub.domain.tld"' >> "$LOG_FILE"
-echo 'zone_id=""' >> "$LOG_FILE"
-echo 'token=""' >> "$LOG_FILE"
-echo 'email=""' >> "$LOG_FILE"
-echo 'proxy="true"' >> "$LOG_FILE"
-echo 'ttl="60"' >> "$LOG_FILE"
+CONF_FILE=./app/script/.env
+echo "Thank you for choosing cloudflare-ddns-webGUI!"
+echo "The conig file is stored in ./app/script/.eng"
+if [ -f "$CONF_FILE" ]; then
+    rm "$CONF_FILE"
+    echo "Old file found, deleting it..."
+fi
+touch "$CONF_FILE"
+echo "Creating the config file and add empty config to it..."
+echo 'record="sub.domain.tld"' >> "$CONF_FILE"
+echo 'zone_id=""' >> "$CONF_FILE"
+echo 'token=""' >> "$CONF_FILE"
+echo 'email=""' >> "$CONF_FILE"
+echo 'proxy="true"' >> "$CONF_FILE"
+echo 'ttl="60"' >> "$CONF_FILE"
 
 # Set permission to the app folder
+echo "Setting permission to the app folder"
 chmod -R 777 ./app 
 
+echo "Now docker compose up, with detach parameter (-d)."
 docker compose up -d
